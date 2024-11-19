c48c17f0729007985fe472f2c257b87101be8cc3
kieranwv <kieranwme@gmail.com>
2024-11-19 16:06:24 +0800

    fix: fix types

--- app/composables/locale.ts
+++ app/composables/locale.ts
@@ -1,15 +1,17 @@
+import type { Locale } from 'vue-i18n'
+
 /**
  * Locale composable for vue-i18n.
  */
 export function useLocale() {
   const { locale, messages } = useI18n()
 
-  function toggleLocale(_locale?: string) {
+  function toggleLocale(_locale?: Locale) {
     if (!_locale) {
       const locales = Object.keys(messages.value)
-      _locale = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+      _locale = locales[(locales.indexOf(locale.value) + 1) % locales.length] as Locale
     }
-    locale.value = _locale as string
+    locale.value = _locale
   }
 
   return {
